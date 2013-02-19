ba8d47b61798d3cf60eabd70ceb44067fedea23b
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-02-18 22:41:18 -0500

    Added TK example

---
+++ examples/tk_slider.py
@@ -0,0 +1,19 @@
+from Tkinter import *
+from phue import Bridge
+
+b = Bridge() # Enter bridge IP here.
+
+#If running for the first time, press button on bridge and run with b.connect() uncommented
+#b.connect()
+
+b.set_light([1,2,3], 'on', True)
+
+def sel(data):
+    b.set_light([1,2,3],{'bri':int(data), 'transitiontime': 1})
+
+root = Tk()
+scale = Scale( root, from_ = 254, to = 0, command= sel, length = 200 )
+scale.set(b.get_light(1,'bri'))
+scale.pack(anchor=CENTER)
+
+root.mainloop()
