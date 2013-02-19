0c12c6b887c2a9345c796476c39f90120a772877
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-02-18 22:10:25 -0500

    Added examples folder

---
+++ examples/random_colors.py
@@ -0,0 +1,16 @@
+from phue import Bridge
+import random
+
+
+b = Bridge() # Enter bridge IP here.
+
+#If running for the first time, press button on bridge and run with b.connect() uncommented
+#b.connect()
+
+lights = b.get_light_objects()
+
+for light in lights:
+	light.brightness = 254
+	light.xy = [random.random(),random.random()]
+
+
