5b79c008b372739f22d4b08bc42c4e4556a93eaf
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-05-25 17:07:05 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@
 from setuptools import setup
 from phue import __version__
 
-readme = open('README.md').read()
-
 setup(name='phue',
       version=__version__,
       author='Nathanaël Lécaudé',
       url='https://github.com/studioimaginaire/phue',
       license='WTFPL',
       description='A Philips Hue Python library',
-      long_description=readme,
+      long_description='A Philips Hue Python library',
       py_modules=['phue'],
       )
