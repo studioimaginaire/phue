d180e5e3578eb3facbcf6116aced81b9abc9e53a
Sean Dague <sean@dague.net>
2016-10-27 06:59:29 -0400

    add initial testing patch
    This creates the first testing patch for phue, there is a basic import
    test, a test of the registration http calls, as well as tox & travis
    setup to make these easy to run.

---
+++ tests/test_request.py
@@ -0,0 +1,44 @@
+# Published under the MIT license - See LICENSE file for more detail
+#
+# This is a basic test file which just tests that things import, which
+# means that this is even vaguely python code.
+
+import fixtures
+import mock
+import os
+import testtools
+
+import phue
+
+
+class TestRequest(testtools.TestCase):
+
+    def setUp(self):
+        super(TestRequest, self).setUp()
+        self.home = fixtures.TempHomeDir()
+        self.useFixture(self.home)
+
+    def test_register(self):
+        """test that registration happens automatically during setup."""
+        confname = os.path.join(self.home.path, '.python_hue')
+        with mock.patch("phue.Bridge.request") as req:
+            req.return_value = [{'success': {'username': 'fooo'}}]
+            bridge = phue.Bridge(ip="10.0.0.0")
+            self.assertEqual(bridge.config_file_path, confname)
+        with open(confname) as f:
+            contents = f.read()
+            self.assertEqual(contents, '{"10.0.0.0": {"username": "fooo"}}')
+
+    def test_register_fail(self):
+        """Test that registration fails in the expected way for timeout"""
+        with mock.patch("phue.Bridge.request") as req:
+            req.return_value = [{'error': {'type': 101}}]
+            self.assertRaises(phue.PhueRegistrationException,
+                              phue.Bridge, ip="10.0.0.0")
+
+    def test_register_unknown_user(self):
+        """Test that registration for unknown user works."""
+        with mock.patch("phue.Bridge.request") as req:
+            req.return_value = [{'error': {'type': 7}}]
+            self.assertRaises(phue.PhueException,
+                              phue.Bridge, ip="10.0.0.0")
