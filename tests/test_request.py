7026ad9cb4110cf6dd66cd1dd2a141bd4d5d5645
Sean Dague <sean@dague.net>
2016-10-27 16:22:49 -0400

    build framework for testing more complex API interactions
    This builds a framework for having a set of canned test responses
    based on HTTP Requests. It does this by monkey patching out
    HTTPConnection, and redirecting it into a static multi level
    dictionary. This allows complicated flows to be tested against known
    sample data, and ensure that we're actually testing the various calls
    back to the server.
    
    A simple use of this in getting the data for one of the lights is
    added. This will be expanded from here once it's accepted as an
    approach.

--- tests/test_request.py
+++ tests/test_request.py
@@ -6,9 +6,16 @@
 import fixtures
 import mock
 import os
+import sys
 import testtools
 
 import phue
+import fakes
+
+if sys.version_info[0] > 2:
+    httplib = 'http.client.HTTPConnection'
+else:
+    httplib = 'httplib.HTTPConnection'
 
 
 class TestRequest(testtools.TestCase):
@@ -53,3 +60,15 @@ class TestRequest(testtools.TestCase):
             req.return_value = [{'error': {'type': 7}}]
             self.assertRaises(phue.PhueException,
                               phue.Bridge, ip="10.0.0.0")
+
+
+class TestLights(testtools.TestCase):
+
+    def setUp(self):
+        super(TestLights, self).setUp()
+        self.useFixture(fixtures.MonkeyPatch(httplib, fakes.FakeHTTP))
+        self.bridge = phue.Bridge(ip="10.0.0.0", username="username")
+
+    def test_get_lights(self):
+        lights = self.bridge.get_light_objects('id')
+        self.assertEqual(lights[1].name, "Living Room Bulb")
